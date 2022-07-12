5a7aff3bf771068e06946f0ff06bfc81ff072150
donglgl <donglgl@naver.com>
2022-07-12 20:26:52 +0900

    리사클러뷰 mainActivity에 연동

---
+++ app/src/main/java/com/webnatues/testiotfirebase/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.webnatues.testiotfirebase
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var adapter:ListAdapter
+    private val viewModel by lazy { ViewModelProvider(this).get(ListViewModel::class.java) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        adapter = ListAdapter(this)
+
+        val recyclerView : RecyclerView = findViewById(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+        observerData()
+
+    }
+
+    fun observerData(){
+        viewModel.fetchData().observe(this, Observer {
+            adapter.setListData(it)
+            adapter.notifyDataSetChanged()
+
+        })
+    }
+}
---
+++ app/src/main/java/com/webnatues/testiotfirebase/User.kt
@@ -0,0 +1,12 @@
+package com.webnatues.testiotfirebase
+
+data class User(
+    var IMG : String = "IMG",
+    var tablenumber:Int = 0,
+    var menu1:String = "Menu1",
+    var menu2:String = "Menu2",
+    var menu3:String = "Menu3",
+    var EA1:Int = 0,
+    var EA2:Int = 0,
+    var EA3:Int = 0
+)
